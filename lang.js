1883737ae6f0d3ecb4bc8011dc1b681bee9b1de7
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-09-14 10:18:38 +0100

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,29 @@
+const LANG_EN_CLAPPER = {
+    ADD_VIDEO: "Add Video",
+    NO_VIDEO_PLAYING: "No video playing"
+};
+
+const LANG_JP_CLAPPER = {
+    ADD_VIDEO: "ビデオを追加",
+    NO_VIDEO_PLAYING: "ビデオが再生されていません"
+};
+
+const LANG_DE_CLAPPER = {
+    ADD_VIDEO: "Video hinzufügen",
+    NO_VIDEO_PLAYING: "Kein Video wird abgespielt"
+};
+
+const LANG_ES_CLAPPER = {
+    ADD_VIDEO: "Añadir video",
+    NO_VIDEO_PLAYING: "No se está reproduciendo ningún video"
+};
+
+const LANG_KO_CLAPPER = {
+    ADD_VIDEO: "비디오 추가",
+    NO_VIDEO_PLAYING: "재생 중인 비디오가 없습니다"
+};
+
+const LANG_ZH_CLAPPER = {
+    ADD_VIDEO: "添加视频",
+    NO_VIDEO_PLAYING: "没有正在播放的视频"
+};
